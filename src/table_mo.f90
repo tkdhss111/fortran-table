9e927c61f3da51c0abdbef1f65a2ced85e2ed498
Hisashi Takeda <tkdhss111@gmail.com>
2025-05-25 12:07:45 +0900

    change LEN_C=24 to 255

--- src/table_mo.f90
+++ src/table_mo.f90
@@ -16,7 +16,7 @@ module table_mo
   public :: count_rows, count_cols, count_seps
   public :: csv2parquet
 
-  integer,          parameter :: LEN_C = 24     ! Character length of each cell
+  integer,          parameter :: LEN_C = 255    ! Character length of each cell
   character(LEN_C), parameter :: cNA   = 'NA'   ! N/A data notation for character
   integer,          parameter :: iNA   = -999   ! N/A data notation for integer
   real,             parameter :: rNA   = -999.0 ! N/A data notation for integer
@@ -61,6 +61,7 @@ module table_mo
 
   end type
 
+  ! Public functions
   interface select
     procedure :: select_integer, select_logical, select_character
   end interface
